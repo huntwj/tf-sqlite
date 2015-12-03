8050199a5a298884382e47bc0569e6ab77f9bca7
Wil Hunt <huntwj@gmail.com>
2015-12-02 21:41:54 -0800

    Getting started with sqlite database support. Implement a raw query method.

---
+++ main.tf
@@ -0,0 +1,16 @@
+;;
+;; tf-sqlite
+;;
+
+/test sqlite_executable := "sqlite3"
+/def sqlite_rawQuery = \
+    /let _file=%{1}%;\
+    /let _query=%{-1}%;\
+    /echo Querying: %{_file}%;\
+    /echo SQL: %{_query}%;\
+    /let _r=$(/quote -S -decho !%{sqlite_executable} %{_file} "%{_query}")%;\
+    /return _r
+
+/def sqlite_escapeSql = \
+    /return replace("'", "''", {*})
+
