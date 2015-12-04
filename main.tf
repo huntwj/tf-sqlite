63bece869a0889da96acbd367fc56eba8c7419f7
Wil Hunt <huntwj@gmail.com>
2015-12-04 04:37:41 -0800

    Comment out logging messages.

--- main.tf
+++ main.tf
@@ -6,9 +6,10 @@
 /def sqlite_rawQuery = \
     /let _file=%{1}%;\
     /let _query=%{-1}%;\
-    /echo Querying: %{_file}%;\
-    /echo SQL: %{_query}%;\
+;    /echo Querying: %{_file}%;\
+;    /echo SQL: %{_query}%;\
     /let _r=$(/quote -S -decho !%{sqlite_executable} %{_file} "%{_query}")%;\
+;    /echo Result: '%{_r}'%;\
     /return _r
 
 /def sqlite_escapeSql = \
